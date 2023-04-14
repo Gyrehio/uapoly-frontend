d1e5ce68768d3286c1c1ee193402cb14594dfdd3
Gyrehio <victor.loiseau2@gmail.com>
2023-04-14 14:20:56 +0200

    Logout button added

--- src/components/UserHeader.tsx
+++ src/components/UserHeader.tsx
@@ -1,21 +1,20 @@
 import React, {Component} from "react";
+import {Link, Navigate, redirect} from 'react-router-dom';
 
 type UserHeaderProps = {};
 
 type UserHeaderState = {
-    login: string
+    login: string,
+    redirectUrl: string | null
 }
 
 class UserHeader extends Component<UserHeaderProps, UserHeaderState> {
     constructor(props) {
         super(props);
 
-        let states: UserHeaderState = {
-            login: ""
-        };
-
         this.state = {
-            login: ""
+            login: "",
+            redirectUrl: null
         };
 
         fetch('/user/me', {
@@ -27,15 +26,29 @@ class UserHeader extends Component<UserHeaderProps, UserHeaderState> {
         })
         .then((result) => result.json())
         .then((res) => this.setState({
-            login: res.login
+            login: res.login,
+            redirectUrl: null
         }));
        
+    }
+
+    disconnect(e) {
+        e.preventDefault();
+        localStorage.removeItem("token");
+        this.setState({
+            login: "",
+            redirectUrl: "/"
+        });
     }
 
     render(): React.ReactNode {
         return (
             <div className="userheader">
+                {this.state.redirectUrl &&
+                    <Navigate to={this.state.redirectUrl} replace={true} />
+                }
                 <h1>Welcome {this.state.login}</h1>
+                <input type="button" name="disconnect" value={"Log out"} onClick={this.disconnect.bind(this)} />
             </div>
         );
     }
