f99e8d2e0e18338a4653a433bf8939123b15515f
Gyrehio <victor.loiseau2@gmail.com>
2023-04-24 16:57:39 +0200

    Change email form is enabled

--- src/components/AccountSettings.tsx
+++ src/components/AccountSettings.tsx
@@ -8,7 +8,10 @@ type AccountSettingsState = {
     email: string,
     oldPassword: string,
     newPassword: string,
-    confirm: string
+    confirmPswd: string,
+    newMail: string,
+    confirmMail: string,
+    pswdMail: string
 };
 
 class AccountSettings extends Component<AccountSettingsProps, AccountSettingsState> {
@@ -20,7 +23,10 @@ class AccountSettings extends Component<AccountSettingsProps, AccountSettingsSta
             email: "",
             oldPassword: "",
             newPassword: "",
-            confirm: ""
+            confirmPswd: "",
+            newMail: "",
+            confirmMail: "",
+            pswdMail: ""
         };
 
         this.update();
@@ -40,17 +46,35 @@ class AccountSettings extends Component<AccountSettingsProps, AccountSettingsSta
 
     handleConfirmPassword(e) {
         this.setState({
-            confirm: e.target.value
+            confirmPswd: e.target.value
+        });
+    }
+
+    handleNewMail(e) {
+        this.setState({
+            newMail: e.target.value
+        });
+    }
+
+    handleConfirmMail(e) {
+        this.setState({
+            confirmMail: e.target.value
+        });
+    }
+
+    handlePassword(e) {
+        this.setState({
+            pswdMail: e.target.value
         });
     }
 
     handleClick(e) {
         e.preventDefault();
 
-        if (this.state.newPassword !== this.state.confirm) {
+        if (this.state.newPassword !== this.state.confirmPswd) {
             this.setState({
                 newPassword: "",
-                confirm: ""
+                confirmPswd: ""
             });
             alert("Please make sure the new password and the confirm password are the same.");
         } else {
@@ -73,9 +97,49 @@ class AccountSettings extends Component<AccountSettingsProps, AccountSettingsSta
                     this.setState({
                         oldPassword: "",
                         newPassword: "",
-                        confirm: ""
+                        confirmPswd: ""
                     });
                     alert(user["message"]);
+                }
+            };
+            
+            loader();
+        }
+    }
+
+    handleClickMail(e) {
+        e.preventDefault();
+
+        if (this.state.newMail !== this.state.confirmMail) {
+            this.setState({
+                newMail: "",
+                confirmMail: ""
+            });
+            alert("Please make sure the new Email address and the confirm Email address are the same.");
+        } else {
+            const obj = JSON.stringify({
+                "email": this.state.newMail,
+                "password": this.state.pswdMail
+            });
+
+            const loader = async () => {
+                const user = await (await fetch('/user/change-email', {
+                    method: "POST",
+                    body: obj,
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": "Bearer ".concat(localStorage.token)
+                    }
+                })).json();
+    
+                if (user) {
+                    this.setState({
+                        newMail: "",
+                        confirmMail: "",
+                        pswdMail: ""
+                    });
+                    alert(user["message"]);
+                    this.update();
                 }
             };
             
@@ -113,31 +177,59 @@ class AccountSettings extends Component<AccountSettingsProps, AccountSettingsSta
                         <label className="infos" htmlFor="emailLabel">Email : {this.state.email}</label>
                     </div>
                 </div>
-                <div className="passwordForm">
-                    <form name="changePassword">
-                        <div>
-                            <label>Change your password</label>
-                        </div>
-                        <br/>
-                        <div>
-                            <label>Actual password</label><br/>
-                            <input type="password" className="passwordForm" name="actualPswd" required onChange={this.handleOldPassword.bind(this)} value={this.state.oldPassword} />
-                        </div>
-                        <br/>
-                        <div>
-                            <label>New password</label><br/>
-                            <input type="password" className="passwordForm" name="newPswd" required onChange={this.handleNewPassword.bind(this)} value={this.state.newPassword} />
-                        </div>
-                        <br/>
-                        <div>
-                            <label>Confirm password</label>
-                            <input type="password" className="passwordForm" name="confirmPswd" required onChange={this.handleConfirmPassword.bind(this)} value={this.state.confirm} />
-                        </div>
-                        <br/>
-                        <div>
-                            <input type="button" name="button" value={"Confirm"} onClick={this.handleClick.bind(this)} />
-                        </div>
-                    </form>
+                <div className="changeForms">
+                    <div className="passwordForm">
+                        <form name="changePassword">
+                            <div>
+                                <label>Change your password</label>
+                            </div>
+                            <br/>
+                            <div>
+                                <label>Actual password</label><br/>
+                                <input type="password" className="passwordForm" name="actualPswd" required onChange={this.handleOldPassword.bind(this)} value={this.state.oldPassword} />
+                            </div>
+                            <br/>
+                            <div>
+                                <label>New password</label><br/>
+                                <input type="password" className="passwordForm" name="newPswd" required onChange={this.handleNewPassword.bind(this)} value={this.state.newPassword} />
+                            </div>
+                            <br/>
+                            <div>
+                                <label>Confirm password</label>
+                                <input type="password" className="passwordForm" name="confirmPswd" required onChange={this.handleConfirmPassword.bind(this)} value={this.state.confirmPswd} />
+                            </div>
+                            <br/>
+                            <div>
+                                <input type="button" name="button" value={"Confirm"} onClick={this.handleClick.bind(this)} />
+                            </div>
+                        </form>
+                    </div>
+                    <div className="emailForm">
+                        <form name="changeEmail">
+                            <div>
+                                <label>Change your Email address</label>
+                            </div>
+                            <br/>
+                            <div>
+                                <label>New Email address</label><br/>
+                                <input type="text" className="emailForm" name="newEmail" required onChange={this.handleNewMail.bind(this)} value={this.state.newMail} />
+                            </div>
+                            <br/>
+                            <div>
+                                <label>Confirm Email address</label><br/>
+                                <input type="text" className="emailForm" name="confirmEmail" required onChange={this.handleConfirmMail.bind(this)} value={this.state.confirmMail} />
+                            </div>
+                            <br/>
+                            <div>
+                                <label>Password</label>
+                                <input type="password" className="emailForm" name="pswd" required onChange={this.handlePassword.bind(this)} value={this.state.pswdMail} />
+                            </div>
+                            <br/>
+                            <div>
+                                <input type="button" name="button" value={"Confirm"} onClick={this.handleClickMail.bind(this)} />
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </>
             
